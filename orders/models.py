66aa70b4b70c53329d58d509115fa5fbeae2893e
Sayeda Khaled <sayedakhaled@Sayedas-MacBook-Pro.local>
2021-06-16 20:12:42 -0400

    created the orders model

--- orders/models.py
+++ orders/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Order(models.Model): # we are inheriting from the model we are inheriting the model from django..
-    img = models.CharField(max_length=255, null=True)
-    name = models.CharField(max_length=255)
-    desc= models.CharField(max_length=255)
-    price = models.CharField(max_length=255)
+    # img = models.CharField(max_length=255, null=True)
+    name = models.CharField(max_length=255, null=True)
+    address = models.CharField(max_length=255, null=True)
+    phone = models.CharField(max_length=255, null=True)
+    product = models.CharField(max_length=255, null=True)
+    total = models.CharField(max_length=255, null=True)
 
 
     def __str__(self):   #This lists the title in the page now.. You don't have to make migration, but you would if you are changing your model, cos this is how the database is updated
