77129df3a79b658b952c97d3227e2b1673c8cc5b
Sayeda Khaled <sayedakhaled@Sayedas-MacBook-Pro.local>
2021-06-17 16:19:16 -0400

    post is working properly

--- orders/models.py
+++ orders/models.py
@@ -6,8 +6,8 @@ class Order(models.Model): # we are inheriting from the model we are inheriting 
     name = models.CharField(max_length=255, null=True)
     address = models.CharField(max_length=255, null=True)
     phone = models.CharField(max_length=255, null=True)
-    product = models.CharField(max_length=255, null=True)
-    total = models.CharField(max_length=255, null=True)
+    product = models.JSONField(null=True)
+    total = models.IntegerField(null=True)
 
 
     def __str__(self):   #This lists the title in the page now.. You don't have to make migration, but you would if you are changing your model, cos this is how the database is updated
